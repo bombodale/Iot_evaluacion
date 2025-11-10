2c7ba9dd7bf33fa1cdaf2db9a7a026086834d3c9
juan <jricardonn16@gmail.com>
2025-11-10 13:55:47 -0300

    coreccion 3

--- app/src/main/java/com/example/evaluacion_iot/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/evaluacion_iot/ForgotPasswordActivity.kt
@@ -1,11 +1,27 @@
 package com.example.evaluacion_iot
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class ForgotPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
+
+
+        val forgotpassButton = findViewById<Button>(R.id.sendRecoveryButton)
+
+        forgotpassButton.setOnClickListener {
+            Toast.makeText(this, "Se le envio un correo", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, Nuevo_user::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
+
+
